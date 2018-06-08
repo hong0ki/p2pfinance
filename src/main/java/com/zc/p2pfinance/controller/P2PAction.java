113bea94a70c130a8ebe9a71204c7eb62785cd36
hong0ki <1791653823@qq.com>
2018-06-08 16:02:18 +0800

    返回结果集

--- src/main/java/com/zc/p2pfinance/controller/P2PAction.java
+++ src/main/java/com/zc/p2pfinance/controller/P2PAction.java
@@ -4,15 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.zc.p2pfinance.po.ResultMessage;
+
 @RequestMapping("/user")
 @Controller
 public class P2PAction {
 
 	@RequestMapping("/register")
 	@ResponseBody
-	public Object register(String string) {
+	public ResultMessage register(String string) {
 		System.out.println(string);
-		
-		return null;
+		String str=string+"结果集";
+		//登录标志，操作结果，反馈信息，结果集
+		return new ResultMessage(true,1,"返回提示信息",str);
 	}
 }
